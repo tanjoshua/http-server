cfe7148c51a98d1b5ef0c57bf3a9121864c82426
Josh <jtanjoshua@gmail.com>
2025-12-30 16:59:39 +0800

    handling http requests better

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,62 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+const BAD_REQUEST_RESPONSE: &str = "HTTP/1.1 400 Bad Request\r\n\r\n";
+const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 Not Found";
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
+        let stream = stream?;
+        handle_connection(stream)?;
     }
 
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> Result<(), String> {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
+
+    let response = handle_request(buf_reader);
+    println!("Sending Response: {}", response);
+    stream.write_all(response.as_bytes()).unwrap();
+
+    Ok(())
+}
+
+fn handle_request(buf_reader: BufReader<&TcpStream>) -> String {
+    let mut http_request = buf_reader
         .lines()
         .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: \r\n{:#?}", http_request);
+        .take_while(|line| !line.is_empty());
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = "Hi, my name is Josh.";
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let Some(request_line) = http_request.next() else {
+        return BAD_REQUEST_RESPONSE.to_string();
+    };
 
-    stream.write_all(response.as_bytes()).unwrap();
+    println!("Request-Line: {}", request_line);
+    let mut request_line = request_line.split_whitespace();
+    let (Some(method), Some(request_uri), Some(http_version)) = (
+        request_line.next(),
+        request_line.next(),
+        request_line.next(),
+    ) else {
+        return BAD_REQUEST_RESPONSE.to_string();
+    };
+
+    if http_version != "HTTP/1.1" {
+        return BAD_REQUEST_RESPONSE.to_string();
+    }
+
+    match (method, request_uri) {
+        ("GET", "/") => {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = "Hi, my name is Josh.";
+            let length = contents.len();
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            response
+        }
+        _ => NOT_FOUND_RESPONSE.to_string(),
+    }
 }
